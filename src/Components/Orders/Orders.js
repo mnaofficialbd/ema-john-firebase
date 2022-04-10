f1164fdd462b617729a2c2418e52b144988b734a
mnaofficial <mna.official92@gmail.com>
2022-04-10 09:39:55 +0600

    file add

---
+++ src/Components/Orders/Orders.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import "./Orders.css"
+
+const Orders = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const navigate = useNavigate();
+    const handleRemoveProduct = (product) => {
+        const rest = cart.filter(pd => pd.id !== product.id);
+        setCart(rest);
+        removeFromDb(product.id)
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="review-items-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                        handleRemoveProduct={handleRemoveProduct}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={() => navigate('/inventory')}>Proceed Checkout</button>
+                    {/* <Link to='/inventory'>
+                        <button onClick={()=>navigate('/inventory')}>Proceed  Cheakout</button>
+                    </Link> */}
+                </Cart>
+            </div>
+
+            {/* <h3>This is Orders: {products.length}</h3>
+            <p>Cart has: {cart.length}</p> */}
+        </div>
+    );
+};
+
+export default Orders;
